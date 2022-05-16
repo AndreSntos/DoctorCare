b42704808f7d8ea416158f084b0a6d9531829646
Andre Santos <andredasil2002@gmail.com>
2022-05-15 23:56:34 -0300

    feat: final website

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
+onScroll();
+window.addEventListener("scroll", onScroll);
+
 function onScroll() {
+  showNavOnScroll();
+  showBackToTopOnScroll();
+  activateMenuCurrentSection(home);
+  activateMenuCurrentSection(services);
+  activateMenuCurrentSection(about);
+}
+
+function showNavOnScroll() {
   if (scrollY > 0) {
     navigation.classList.add("scroll");
   } else {
     navigation.classList.remove("scroll");
+  }
+}
+
+function showBackToTopOnScroll() {
+  if (scrollY > 550) {
+    backToTopButton.classList.add("show");
+  } else {
+    backToTopButton.classList.remove("show");
   }
 }
 
@@ -12,6 +31,23 @@ function openMenu() {
 
 function closeMenu() {
   document.body.classList.remove("menu-expanded");
+}
+
+function activateMenuCurrentSection(section) {
+  const targetLine = scrollY + innerHeight / 2;
+  const sectionTop = section.offsetTop;
+  const sectionHeight = section.offsetHeight;
+  const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop;
+  const sectionEndAt = sectionTop + sectionHeight;
+  const sectionEndPassedTargetLine = sectionEndAt <= targetLine;
+  const sectionBoundaries =
+    sectionTopReachOrPassedTargetLine && !sectionEndPassedTargetLine;
+  const sectionId = section.getAttribute("id");
+  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`);
+  menuElement.classList.remove("active");
+  if (sectionBoundaries) {
+    menuElement.classList.add("active");
+  }
 }
 
 ScrollReveal({
